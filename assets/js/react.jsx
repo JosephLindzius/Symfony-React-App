dd5433d1d352277b7f6783cfcaebaed1bb574a77
JosephLindzius <joseph.lindzius@gmail.com>
2020-01-19 15:35:19 +0100

    added first react dep

---
+++ assets/js/react.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import RenderDom from 'react-dom'
+
+function React () {
+
+}
--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Limenius\ReactBundle\LimeniusReactBundle::class => ['all' => true],
 ];
